e04244ffce5c85efc25896e7abcb7af9df9ac975
abualrub03 <89502136+abualrub03@users.noreply.github.com>
2024-04-28 12:54:11 +0300

    app added

---
+++ MaaHayat/MaaHayat/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using MaaHayat.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+namespace MaaHayat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MainMapPage()
+        {
+
+            return View();
+        }
+
+    }
+}
